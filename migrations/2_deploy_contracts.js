a4e1b5283f8c59daca7ebdb0e9d5b62451ff4671
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2021-05-26 20:51:14 +0530

    Commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) 
+{
+    //Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed() 
+
+    //Deploy EthSwap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    //Transfer all token to EthSwap('1 Million')
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+};
